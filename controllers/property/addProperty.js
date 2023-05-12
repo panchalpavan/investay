0927bf89837672afc9e9e80e7e49d2c0f2f063e5
Pavan Panchal <luckiepanchal@gmail.com>
2023-05-12 16:21:27 +0530

    changes

--- controllers/property/addProperty.js
+++ controllers/property/addProperty.js
@@ -6,7 +6,7 @@ const User = require("../../models/User");
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "public/uploads/");
+    cb(null, "uploads/");
   },
   filename: (req, file, cb) => {
     cb(null, Date.now() + file.originalname);
